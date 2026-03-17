40b883ebcb1ca6166d8738d10e53360d0b692cbd
Sanket <sanketkarki2060@gmail.com>
2026-03-17 09:22:58 +0545

    fix:fix build error

--- src/components/auth0login.jsx
+++ src/components/auth0login.jsx
@@ -5,6 +5,7 @@ const LoginButton = () => {
   const { loginWithRedirect, user } = useAuth0();
 
   return (
+    <></>
     // <button
     //   className="align-middle font-bold font-sans mt-1 p-2 text-center h-10 w-full bg-blue-700 text-white rounded-full focus:opacity-[0.85] focus:shadow-none active:opacity-[0.85] active:shadow-none text-xs"
     //   onClick={async () => {
